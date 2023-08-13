f0c51f91ce7451a93e2c2308fbf7d41b2762c03a
JonZudell <jonathon.zudell@gmail.com>
2023-08-13 12:21:09 -0500

    kubeconfig

--- root/kubernetes-config.tf
+++ root/kubernetes-config.tf
@@ -26,7 +26,7 @@ resource "local_file" "kubeconfig" {
   }
   depends_on = [digitalocean_kubernetes_cluster.fugue-state-cluster]
   content    = digitalocean_kubernetes_cluster.fugue-state-cluster.kube_config[0].raw_config
-  filename   = "${path.root}/.sensitive/kubeconfig"
+  filename   = "${path.root}/../.sensitive/kubeconfig"
 }
 
 data "digitalocean_loadbalancer" "fugue-state-cluster-loadbalancer" {

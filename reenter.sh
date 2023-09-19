884e139f9b6468a5b040148ab6941b7d826935e5
Jon Zudell <jonathon.zudell@gmail.com>
2023-09-19 19:20:48 +0000

    reenter

--- reenter.sh
+++ reenter.sh
@@ -5,8 +5,8 @@ export WORKING_DIRECTORY=$(dirname $BOOTSTRAP_PATH)
 export KUBECONFIG="$WORKING_DIRECTORY/.sensitive/kubeconfig"
 cd $WORKING_DIRECTORY
 export TF_VAR_do_token=""
-export TF_VAR_=""
-export TF_VAR_=""
+export TF_VAR_oauth_client_id=""
+export TF_VAR_oauth_client_secret=""
 export TF_VAR_=""
 export TF_VAR_=""
 export TF_VAR_=""

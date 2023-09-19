9e493a79a658922869e05373b173ee0304a8f3b0
Jon Zudell <jonathon.zudell@gmail.com>
2023-09-19 19:16:33 +0000

    reenter

--- reenter.sh
+++ reenter.sh
@@ -4,4 +4,14 @@ export WORKING_DIRECTORY=$(dirname $BOOTSTRAP_PATH)
 
 export KUBECONFIG="$WORKING_DIRECTORY/.sensitive/kubeconfig"
 cd $WORKING_DIRECTORY
+export TF_VAR_=""
+export TF_VAR_=""
+export TF_VAR_=""
+export TF_VAR_=""
+export TF_VAR_=""
+export TF_VAR_=""
+export TF_VAR_=""
+export TF_VAR_=""
+export TF_VAR_=""
+export TF_VAR_=""
 terraform -chdir=root 

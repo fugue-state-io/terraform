edcf73dd3c57fce138a133c8710471668d8f094b
Jon Zudell <jonathon.zudell@gmail.com>
2023-09-23 03:01:17 +0000

    rerernter

--- reenter.sh
+++ reenter.sh
@@ -7,7 +7,6 @@ cd $WORKING_DIRECTORY
 
 export DO_SPACES_ACCESS_KEY="$(kubectl get secret -n argo-workflows argo-workflows-spaces -o json | jq -r '.data | map_values(@base64d) | ."spaces_access_id"')"
 export DO_SPACES_SECRET_KEY="$(kubectl get secret -n argo-workflows argo-workflows-spaces -o json | jq -r '.data | map_values(@base64d) | ."spaces_secret_key"')"
-export DIGITALOCEAN_TOKEN=""
 
 export TF_VAR_do_token="$DIGITAL_OCEAN_TOKEN"
 export TF_VAR_do_spaces_access_id="$DO_SPACES_ACCESS_KEY"
@@ -16,8 +15,11 @@ terraform init --backend-config="access_key=$DO_SPACES_ACCESS_KEY" --backend-con
 
 export TF_VAR_oauth_client_id="$(kubectl get secret -n argocd fugue-state-argocd-secret -o json | jq -r '.data | map_values(@base64d) | ."dexId"')"
 export TF_VAR_oauth_client_secret="$(kubectl get secret -n argocd fugue-state-argocd-secret -o json | jq -r '.data | map_values(@base64d) | ."dexSecret"')"
-export TF_VAR_argocd_webhook_secret="$(kubectl get secret -n argocd argo-workflows-sso -o json | jq -r '.data | map_values(@base64d) | ."client-id"')"
-export TF_VAR_argo_workflows_client_id="$(kubectl get secret -n argocd argo-workflows-sso -o json | jq -r '.data | map_values(@base64d) | ."client-id"')"
-export TF_VAR_argo_workflows_client_secret="$(kubectl get secret -n argocd argo-workflows-sso -o json | jq -r '.data | map_values(@base64d) | ."client-secret"')"
-export TF_VAR_github_webhook_secret="$(kubectl get secret -n argocd ci-secrets -o json | jq -r '.data | map_values(@base64d) | ."github-webhook-secret"')"
+
+export TF_VAR_argocd_webhook_secret="$(kubectl get secret -n argo-workflows argo-workflows-sso -o json | jq -r '.data | map_values(@base64d) | ."client-id"')"
+export TF_VAR_argo_workflows_client_id="$(kubectl get secret -n argo-workflows argo-workflows-sso -o json | jq -r '.data | map_values(@base64d) | ."client-id"')"
+export TF_VAR_argo_workflows_client_secret="$(kubectl get secret -n argo-workflows argo-workflows-sso -o json | jq -r '.data | map_values(@base64d) | ."client-secret"')"
+
+export TF_VAR_github_webhook_secret="$(kubectl get secret -n ci ci-secrets -o json | jq -r '.data | map_values(@base64d) | ."github-webhook-secret"')"
+
 terraform -chdir=root 

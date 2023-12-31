2b8caf7c9552d27d503b04662fe3da177fe74359
Jon Zudell <jonathon.zudell@gmail.com>
2023-12-31 23:09:40 +0000

    fugue-state-cdn

--- root/s3.tf
+++ root/s3.tf
@@ -2,3 +2,20 @@ resource "digitalocean_spaces_bucket" "fugue-state-s3" {
   name   = "fugue-state-artifacts"
   region = "nyc3"
 }
+
+resource "digitalocean_spaces_bucket" "fugue-state-cdn" {
+  name   = "fugue-state-cdn"
+  region = "nyc3"
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
+  lifecycle_rule {
+  abort_incomplete_multipart_upload_days = 1
+  enabled = true
+    expiration {
+      days = 1
+    }
+}

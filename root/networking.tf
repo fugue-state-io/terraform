b69bc2a6365ddd087ee7fa2985851b19840d1931
Jon Zudell <jonathon.zudell@gmail.com>
2024-02-26 18:27:29 +0000

    dns for zudell.io

--- root/networking.tf
+++ root/networking.tf
@@ -4,6 +4,24 @@ resource "digitalocean_vpc" "fugue-state-vpc" {
   timeouts {}
 }
 
+resource "digitalocean_domain" "zudell-io" {
+  depends_on = [ data.digitalocean_loadbalancer.fugue-state-cluster-loadbalancer ]
+  name = "zudell.io"
+}
+resource "digitalocean_record" "a-zudell-io" {
+  depends_on = [ data.digitalocean_loadbalancer.fugue-state-cluster-loadbalancer ]
+  domain = digitalocean_domain.zudell-io.id
+  type   = "A"
+  name   = "*"
+  value  = data.digitalocean_loadbalancer.fugue-state-cluster-loadbalancer.ip
+}
+resource "digitalocean_record" "at-zudell-io" {
+  domain = digitalocean_domain.zudell-io.id
+  type   = "A"
+  name   = "@"
+  value  = data.digitalocean_loadbalancer.fugue-state-cluster-loadbalancer.ip
+}
+
 resource "digitalocean_domain" "fugue-state" {
   depends_on = [ data.digitalocean_loadbalancer.fugue-state-cluster-loadbalancer ]
   name = "fugue-state.io"

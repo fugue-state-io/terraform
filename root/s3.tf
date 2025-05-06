dccf4505fad45f9cb184abe0198cfa7d9d342176
Jon Zudell <jonathon.zudell@gmail.com>
2025-05-06 12:16:56 -0500

    updated s3 config

--- root/s3.tf
+++ root/s3.tf
@@ -3,7 +3,13 @@ resource "digitalocean_spaces_bucket" "fugue-state-s3" {
   region = "nyc3"
 }
 resource "digitalocean_spaces_bucket" "fugue-state-cdn" {
-  name   = "fugue-state-cdn"
+  name = "fugue-state-cdn"
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "POST", "PUT", "DELETE"]
+    allowed_origins = ["http://localhost:3000", "https://fugue-state.io"]
+    max_age_seconds = 30000
+  }
   region = "nyc3"
   lifecycle_rule {
     abort_incomplete_multipart_upload_days = 1

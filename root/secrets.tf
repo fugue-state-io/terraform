cda70a608db3e215212ec07d15179468e73ee736
Jon Zudell <jonathon.zudell@gmail.com>
2025-05-07 13:52:48 -0500

    secrets

--- root/secrets.tf
+++ root/secrets.tf
@@ -40,7 +40,7 @@ resource "kubernetes_secret" "fugue-state-ui-secrets" {
   data = {
     "AUTH_SECRET"                           = var.nextauth_secret
     "AUTH_URL"                              = var.ui_auth_url
-    "AUTH_URL_INTERNAL"                     = var.nextauth_url
+    "AUTH_URL_INTERNAL"                     = var.ui_auth_url
     "AUTH_KEYCLOAK_ID"                      = var.keycloak_id
     "AUTH_KEYCLOAK_SECRET"                  = var.keycloak_secret
     "AUTH_KEYCLOAK_ISSUER"                  = var.keycloak_issuer

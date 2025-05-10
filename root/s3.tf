b258c223ecb554fad5b8ab63d89ce57bf0e43262
Jon Zudell <jonathon.zudell@gmail.com>
2025-05-10 08:25:21 -0500

    s3 dev bucket and cdn for speed

--- root/s3.tf
+++ root/s3.tf
@@ -2,21 +2,49 @@ resource "digitalocean_spaces_bucket" "fugue-state-s3" {
   name   = "fugue-state-artifacts"
   region = "nyc3"
 }
+
 resource "digitalocean_spaces_bucket" "fugue-state-cdn" {
-  name = "fugue-state-cdn"
+  name   = "fugue-state-cdn"
+  region = "nyc3"
+
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET", "POST", "PUT", "DELETE"]
-    allowed_origins = ["http://localhost:3000", ]
+    allowed_origins = ["http://localhost:3000"]
     max_age_seconds = 30000
   }
+
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET", "POST", "PUT", "DELETE"]
     allowed_origins = ["https://fugue-state.io"]
     max_age_seconds = 30000
   }
+
+  lifecycle_rule {
+    abort_incomplete_multipart_upload_days = 1
+    enabled                                = true
+  }
+}
+
+resource "digitalocean_spaces_bucket" "fugue-state-cdn-dev" {
+  name   = "fugue-state-cdn-dev"
   region = "nyc3"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "POST", "PUT", "DELETE"]
+    allowed_origins = ["http://localhost:3000"]
+    max_age_seconds = 30000
+  }
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "POST", "PUT", "DELETE"]
+    allowed_origins = ["https://dev.fugue-state.io"]
+    max_age_seconds = 30000
+  }
+
   lifecycle_rule {
     abort_incomplete_multipart_upload_days = 1
     enabled                                = true

5389c1feb64357a86ae8a7c0c25197a1e6d08238
Jon Zudell <jonathon.zudell@gmail.com>
2025-05-12 10:58:06 -0500

    modified secrets and secrets backup

--- scripts/source_config.sh
+++ scripts/source_config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 doctl kubernetes cluster kubeconfig save fugue-state-cluster
-kubectl get secret -n tf-backup tf-backup -o json | jq -r '.data | map_values(@base64d)'
+kubectl get secret -n tf-backup tf-secrets -o json | jq -r '.data | map_values(@base64d)'
 
-for key in $(kubectl get secret -n tf-backup tf-backup -o json | jq -r '.data | keys[]'); do
-  value=$(kubectl get secret -n tf-backup tf-backup -o json | jq -r ".data[\"$key\"]" | base64 --decode)
+for key in $(kubectl get secret -n tf-backup tf-secrets -o json | jq -r '.data | keys[]'); do
+  value=$(kubectl get secret -n tf-backup tf-secrets -o json | jq -r ".data[\"$key\"]" | base64 --decode)
   export "$key"="$value"
 done

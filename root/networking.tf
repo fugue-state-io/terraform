c04b592d8dd909865545375df3bc8f198855b39d
Jon Zudell <jonathon.zudell@gmail.com>
2025-08-09 09:58:09 -0500

    cname for bing site

--- root/networking.tf
+++ root/networking.tf
@@ -138,7 +138,7 @@ resource "digitalocean_record" "google_site_verification" {
 
 resource "digitalocean_record" "bing_site_verification" {
   domain = digitalocean_domain.fugue-state.id
-  type   = "TXT"
-  name   = "@"
-  value  = "a3c7e94751bb14de8973b3f023ef780c"
+  type   = "CNAME"
+  name   = "a3c7e94751bb14de8973b3f023ef780c"
+  value  = "verify.bing.com"
 }
